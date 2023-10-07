72a6aa2513aa622ccbfee05eab74c545bbc11894
nekonabesan <sheepdogs_2@hotmail.com>
2023-10-07 17:58:28 +0900

    EV3からBuild HATへ移行を開始する

--- api/service/connection/sensors/gyro/set/modes.py
+++ api/service/connection/sensors/gyro/set/modes.py
@@ -2,8 +2,8 @@ import api.service.connection.connect_ev3_dev as connect
 
 class SetModes():
     PATH='/sys/class/lego-sensor/'
-    # echo "GYRO-G&A" > /sys/class/lego-sensor/sensor2/mode && cat /sys/class/lego-sensor/sensor2/mode
-    # echo "GYRO-ANG" > /sys/class/lego-sensor/sensor2/mode && cat /sys/class/lego-sensor/sensor2/mode
+    # echo "GYRO-G&A" > /sys/class/lego-sensor/sensor0/mode && cat /sys/class/lego-sensor/sensor2/mode
+    # echo "GYRO-ANG" > /sys/class/lego-sensor/sensor0/mode && cat /sys/class/lego-sensor/sensor2/mode
     def command(self, connection: connect.connectEv3Dev, sensor_id: str):
         connection.sendForSet('echo "GYRO-CAL" > ' + self.PATH  + sensor_id + '/mode && ' 
                     + 'cat ' + self.PATH + sensor_id + '/mode')
